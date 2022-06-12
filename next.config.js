d67fbaf0bd6623de182075f196ec9807845092d2
Meghna <meghna.cvsajikumar@gmail.com>
2022-06-12 20:23:22 +0530

    Last commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images:{
-    domains: ['links.papareact.com']
-  }
-}
+    domains: ['links.papareact.com'],
+  },
+/* env: {
+  mapbox_key:"pk.eyJ1IjoibWVnaG5hc2FqaSIsImEiOiJjbDJzczJpdDQwMXB6M2NscnptcDFoOHRyIn0.XGXsJ-aNoELc72zlN4la5g"
+} */
+
+};
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,62 +1,57 @@
-import {format } from 'date-fns';
+import { format } from 'date-fns'
 import { useRouter } from 'next/router'
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import InfoCard from '../components/InfoCard';
+import InfoCard from '../components/InfoCard'
 
-  
-function Search({searchResults}) {
-
-const router = useRouter();
-const { location, startDate, endDate, noOfGuests } = router.query;
-const formattedStartDate = format (new Date(startDate), "dd MMMM yy");
-const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-const range = `${formattedStartDate} - ${formattedEndDate}`;
-
-
+function Search({ searchResults }) {
+  const router = useRouter()
+  const { location, startDate, endDate, noOfGuests } = router.query
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy')
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy')
+  const range = `${formattedStartDate} - ${formattedEndDate}`
 
   return (
     <div>
-<Header placeholder={`${location} | ${range} | ${noOfGuests} Guests`} />
-<main className="flex">
-    <section className="flex-grow pt-14 px-6">
-        <p className="text-xs">300+ Stays - { range } -for  {noOfGuests}  number of guests</p>
-    <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in {location}</h1>
-    
-    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-        <p className="button">Cancellation Flexibility</p>
-        <p className="button">Type of Places</p>
-        <p className="button">Price</p>
-        <p className="button">Rooms and Beds</p>
-        <p className="button">More Filters</p>
-    </div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} Guests`} />
+      <main className="flex">
+        <section className="flex-grow px-6 pt-14">
+          <p className="text-xs">
+            300+ Stays - {range} -for {noOfGuests} number of guests
+          </p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">
+            Stay in {location}
+          </h1>
 
-<div className="flex flex-col">
-  {searchResults.map(({ img, location, title, description, star, price, total }) => (
-    <InfoCard 
+          <div className="mb-5 hidden space-x-3 whitespace-nowrap text-gray-800 lg:inline-flex">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Places</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
+          </div>
 
-    key={img}
-    img={img} 
-    location={location}
-    title={title}
-     description={description}
-      star={star}
-       price={price}
-        total={total}
-        />
-))}
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
 
-</div>
-
-
-    </section>
-
-
-</main>
-
-<Footer />
-
+      <Footer />
     </div>
   )
 }
@@ -64,13 +59,13 @@ const range = `${formattedStartDate} - ${formattedEndDate}`;
 export default Search
 
 export async function getServerSideProps() {
-    const searchResults = await fetch("https://links.papareact.com/isz").then(res => res.json());
+  const searchResults = await fetch('https://links.papareact.com/isz').then(
+    (res) => res.json()
+  )
 
-return{
-    props:{
-        searchResults,
-    }
-}    
-
+  return {
+    props: {
+      searchResults,
+    },
+  }
 }
-
